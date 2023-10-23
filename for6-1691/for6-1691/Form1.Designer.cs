05fa363c84494c3e43e99cde6b3e1a098f02eef8
sudenazakcicek <148755685+sudenazakcicek@users.noreply.github.com>
2023-10-23 15:45:52 +0300

    Add files via upload

---
+++ for6-1691/for6-1691/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace for6_1691
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBaslangic = new System.Windows.Forms.TextBox();
+            this.txtBitis = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lbTek = new System.Windows.Forms.ListBox();
+            this.lbCift = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtBaslangic
+            // 
+            this.txtBaslangic.Location = new System.Drawing.Point(70, 53);
+            this.txtBaslangic.Name = "txtBaslangic";
+            this.txtBaslangic.Size = new System.Drawing.Size(100, 20);
+            this.txtBaslangic.TabIndex = 0;
+            // 
+            // txtBitis
+            // 
+            this.txtBitis.Location = new System.Drawing.Point(70, 107);
+            this.txtBitis.Name = "txtBitis";
+            this.txtBitis.Size = new System.Drawing.Size(100, 20);
+            this.txtBitis.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(81, 158);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "YAZ";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lbTek
+            // 
+            this.lbTek.FormattingEnabled = true;
+            this.lbTek.Location = new System.Drawing.Point(253, 52);
+            this.lbTek.Name = "lbTek";
+            this.lbTek.Size = new System.Drawing.Size(120, 134);
+            this.lbTek.TabIndex = 3;
+            // 
+            // lbCift
+            // 
+            this.lbCift.FormattingEnabled = true;
+            this.lbCift.Location = new System.Drawing.Point(400, 52);
+            this.lbCift.Name = "lbCift";
+            this.lbCift.Size = new System.Drawing.Size(118, 134);
+            this.lbCift.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbCift);
+            this.Controls.Add(this.lbTek);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtBitis);
+            this.Controls.Add(this.txtBaslangic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBaslangic;
+        private System.Windows.Forms.TextBox txtBitis;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lbTek;
+        private System.Windows.Forms.ListBox lbCift;
+    }
+}
+
---
+++ for6-1691/for6-1691/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for6_1691
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            lbTek.Items.Clear();
+            lbCift.Items.Clear();
+
+            int baslangic = Convert.ToInt32(txtBaslangic.Text);
+            int bitis = Convert.ToInt32(txtBitis.Text);
+
+            for (int i = baslangic; i < bitis; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    lbTek.Items.Add(i);
+
+                }
+                else
+                {
+                    lbCift.Items.Add(i);
+                }
+
+
+            }
+        }
+    }
+}
